552b5ed2ed5a0d7fdb687d8ebe8d2cac7752eb78
Wasin Wongkum <kkmonster@hotmail.com>
2017-07-25 23:28:24 +0700

    get from ID 3

--- Arduino-dev-sensor/Arduino-dev-sensor/Arduino-dev-sensor.ino
+++ Arduino-dev-sensor/Arduino-dev-sensor/Arduino-dev-sensor.ino
@@ -263,7 +263,7 @@ String netpieJsonString;
 
 long getSleepTimeFromNetpie() {
   Serial.println(F("Send HTTP GET"));
-  http.url("https://api.netpie.io/topic/SmartTrash/time/master/2?retain&auth=xTsWAyTWJk3Ba5h:3UzQJ3DeGT50PwfwlmJE0vQF9");
+  http.url("https://api.netpie.io/topic/SmartTrash/time/master/3?retain&auth=xTsWAyTWJk3Ba5h:3UzQJ3DeGT50PwfwlmJE0vQF9");
   Serial.println(http.get());
   // Serial.println(F("Clear data in RAM"));
   file.Delete(RAM, "*");
